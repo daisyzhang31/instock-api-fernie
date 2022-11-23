228eb666b0d5eb6d12333da4ceb8d0663a59037f
欣欣 <cindy@xinxindeMBP.hitronhub.home>
2022-11-22 17:40:45 -0800

    database setup

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const warehouseRoutes = require("./routes/warehouse");
 const PORT = process.env.PORT || 5050;
+const warehouseRoutes = require("./routes/warehouse");
+const inventoryRoutes = require("./routes/inventory");
+
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API");
 });
 
 app.use("/warehouses", warehouseRoutes);
+app.use("/inventories", inventoryRoutes);
 
 app.listen(PORT, () => {
+
   console.log(`running at http://localhost:${PORT}`);
 });

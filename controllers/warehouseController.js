ac876abe8a8a17bbfe6fcf3cf3879a5f4b293d2f
欣欣 <cindy@xinxindeMBP.hitronhub.home>
2022-11-24 17:38:41 -0800

    postwarehouse backend

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,4 +1,7 @@
 const knex = require("knex")(require("../knexfile"));
+const { v4:uuidv4 } =require("uuid");
+
+
 
 exports.index = (_req, res) => {
   knex("warehouses")
@@ -30,3 +33,38 @@ exports.warehouseById = async (req, res) => {
     res.status(400).send(`Error retrieving data: ${err}`);
   }
 };
+
+exports.addWarehouse = (req, res) => {
+  if (
+    !req.body.warehouse_name ||
+    !req.body.address ||
+    !req.body.city ||
+    !req.body.country ||
+    !req.body.contact_name ||
+    !req.body.contact_position ||
+    !req.body.contact_phone ||
+    !req.body.contact_email
+  ) {
+    return res
+      .status(400)
+      .send(
+        "Please make sure to provide warehouse_name, address, city, country, contact_name, contact_position, contact_phone and contact_email fields in a request"
+      );
+  }
+
+  const newWarehouseId = uuidv4();
+  knex("warehouses")
+    .insert({ ...req.body, id: newWarehouseId })
+    .then((_data) => {
+      knex("warehouses")
+        .where({ id: newWarehouseId })
+        .then((data) => {
+          res.status(201).json(data[0]);
+        });
+    })
+    .catch((err) => res.status(400).send(`Error creating Warehouse: ${err}`));
+};
+
+
+
+

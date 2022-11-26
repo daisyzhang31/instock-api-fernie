6c8cc84bffec47b88690837be8ab6c523be67d6f
欣欣 <cindy@xinxindeMBP.hitronhub.home>
2022-11-26 00:54:47 -0800

    addinventory function updated

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,4 +1,5 @@
 const knex = require("knex")(require("../knexfile"));
+const { v4: uuidv4 } = require("uuid");
 
 exports.index = (_req, res) => {
   knex("inventories")
@@ -22,22 +23,24 @@ exports.inventoriesById = async (req, res) => {
   }
 };
 
+
 exports.addInventory = (req, res) => {
   if (
     !req.body.warehouse_id ||
     !req.body.item_name ||
     !req.body.description ||
     !req.body.category ||
-    !req.body.status
+    !req.body.status ||
+    !req.body.quantity
   ) {
     return res
       .status(400)
       .send(
-        "Please make sure to provide warehouse_name, item-name,description,category,and status in a request"
+        "Please provide warehouseid, itemname, description,category, status and quantity."
       );
   }
-
   const newInventoryId = uuidv4();
+
   knex("inventories")
     .insert({ ...req.body, id: newInventoryId })
     .then((_data) => {
@@ -47,51 +50,6 @@ exports.addInventory = (req, res) => {
           res.status(201).json(data[0]);
         });
     })
-    .catch((err) => res.status(400).send(`Error creating Inventory: ${err}`));
+    .catch((err) => res.status(400).send(`Error creating inventory:${err}`));
 };
 
-
-// exports.updateInventory = (req, res) => {
-//   if (
-//     !req.body.id ||
-//     !req.body.warehouse_id ||
-//     !req.body.item_name ||
-//     !req.body.description ||
-//     !req.body.category ||
-//     !req.body.status ||
-//     !req.body.quantity ||
-//     !req.body.created_at ||
-//     !req.body.updated_at
-//   ) {
-//     return res
-//       .status(400)
-//       .send(
-//         "Please make sure to provide warehouse_name, address, city, country, contact_name, contact_position, contact_phone and contact_email fields in a request"
-//       );
-//   }
-
-//   knex("inventories");
-//   const obj = {
-//     id: req.body.id,
-//     warehouse_id: req.body.warehouse_id,
-//     item_name: req.body.item_name,
-//     description: req.body.description,
-//     category: req.body.category,
-//     status: req.body.status,
-//     quantity: req.body.quantity,
-//     created_at: req.body.created_at,
-//     updated_at: req.body.updated_at,
-//   }
-//     .update(obj)
-//     .where({ id: req.params.id })
-//     .then((_data) => {
-//       knex("inventories")
-//         .where({ id: req.params.id })
-//         .then((data) => {
-//           res.status(200).json(data[0]);
-//         });
-//     })
-//     .catch((err) =>
-//       res.status(400).send(`Error updating Warehouse ${req.params.id} ${err}`)
-//     );
-// };
